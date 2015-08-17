6e5bcbe9f0d0da6052e900439ec86cad2618f02b
chitra atmakuru & mala karpy <student@epicodus.com>
2015-08-17 10:20:47 -0700

    Add initial files

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle {
+  public static void main(String[] args) {}
+    private Integer mLength;
+    private Integer mWidth;
+    public Rectangle(Integer length, Integer width) {
+      mLength = length;
+      mWidth = width;
+     }
+     public int getLength() {
+       return mLength;
+     }
+     public int getWidth() {
+      return mWidth;
+     }
+
+     public boolean isSquare() {
+       return mLength == mWidth;
+
+     }
+}
